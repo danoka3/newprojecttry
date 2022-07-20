467e71e80e1a49a9e717c613b525f695e5755b52
danoka3 <batuhan.tanriover@lojiplatform.com>
2022-07-20 10:16:06 +0000

    tıss

---
+++ odoo_library/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Library Managament',
+    
+    'summary': """An app for library management""",
+    
+    'description': """
+    ---
+    
+    """,
+    
+    'author': 'Batuhan Tanrıöver',
+    
+    'website': 'https://www.odoo.com',
+    
+    'version': '0.1',
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+      
+        
+        
+    ],
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
