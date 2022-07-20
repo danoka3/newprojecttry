db5c78321799a4ad3d604ddd4821827181782eca
danoka3 <batuhan.tanriover@lojiplatform.com>
2022-07-20 11:29:13 +0000

    fields

--- odoo_library/models/library.py
+++ odoo_library/models/library.py
@@ -5,4 +5,18 @@ from odoo import models, fields, api
 class Library(models.Model):
     
     _name = "library.book"
-    _description = "App for library management"
+    _description = "Library Info"
+    
+    
+    name = fields.Char(string="Title" , required=True)
+    descriptipn = fields.Text(string="Description")
+    author = fields.Char(string="Author Name", required=True)
+    editor = fields.Char(string="Editor")
+    year_of_edition = fields.Date("Date")
+    genre = fields.Selection(string="Genre",
+                            selection=[("action","Action"),
+                                      ("novel","Novel"),
+                                      ("self-improvement","Self-improvement")],
+                            copy=False)
+    
+    active = fields.Boolean(string="Active", default=True)
